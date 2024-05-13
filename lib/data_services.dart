6f111a86540e9b8506140a933fb2e895ff0275a3
Boitumelo29 <77927688+Boitumelo29@users.noreply.github.com>
2024-05-13 08:13:53 +0200

    update

--- lib/data_services.dart
+++ lib/data_services.dart
@@ -11,4 +11,13 @@ class DataServices {
   }
 }
 
-class DataServces {}
+class DataSeces {
+  Future getJoke() async {
+    //https://icanhazdadjoke.com/
+
+    final uri = Uri.https('icanhazdadjoke.com');
+    print(uri);
+    final response = await http.get(uri);
+    print("my response: ${response.body}");
+  }
+}
